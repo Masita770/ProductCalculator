de44ef978faf75c6835d409e8d99d2985e4627e5
Masita770 <ozon777ym12@gmail.com>
2024-01-10 22:41:36 +0900

    fix: UserMapper・UserService・UserController・list.html・form.html・user.html 修正

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.example.mapper;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.domain.Products;
 import com.example.domain.Users;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -12,27 +13,27 @@ public interface UserMapper {
 
     //全表示（Read処理)
     //@Select("SELECT * FROM users ")
-    List<Users> selectAll();
+    List<Products> selectAll();
 
     //一件表示
     //@Select("SELECT id, username, password FROM users WHERE id = #{id}")
-    Optional<Users> selectOne(Integer id);
+    Optional<Products> selectOne(Integer id);
 
 
     //Insert処理
     //@Insert("INSERT INTO users (id,username,password) values (#{id},#{username},#{password})")
-    void add(Users user);
+    void add(Products products);
 
     //Update処理
     //@Update("UPDATE users set username = #{username}, password = #{password} where id = #{id}")
-    void update(Users update);
+    void update(Products update);
 
     //Delete処理
     //@Delete("DELETE FROM users WHERE id = #{id}")
-    void delete(Users delete);
+    void delete(Products delete);
 
     Users certificate(Integer id);
 
-    void make(Users user);
+    void make(Products products);
 }
 
