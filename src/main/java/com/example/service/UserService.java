cdcdc06a8f7048fc249dcbbf1464e8c183f5ee55
Masita770 <ozon777ym12@gmail.com>
2024-03-12 23:29:56 +0900

    fix: UserService 修正

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -33,7 +33,7 @@ public class UserService {
 //        return mapper.selectName(username);
 //    }
 
-    //Insert処理
+    //Insert処理　3/12
     public void create(Orders products) {
 //        int productId = products.getId();
 //        products.setId(productId);
