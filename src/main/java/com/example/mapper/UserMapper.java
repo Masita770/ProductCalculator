b13978b3f04fca9859ea5ea84ffdb2e741aad3cc
Masita770 <ozon777ym12@gmail.com>
2024-04-24 23:23:41 +0900

    fix: UserMapper.java 修正

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -23,7 +23,7 @@ public interface UserMapper {
 
     //Insert処理
     //@Insert("INSERT INTO users (id,username,password) values (#{id},#{username},#{password})")
-    void add(Products products);
+    void add(Orders products);
 
     //Update処理
     //@Update("UPDATE users set username = #{username}, password = #{password} where id = #{id}")
