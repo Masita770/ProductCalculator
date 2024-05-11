37a0bf402cf401e776593bf4b77be06bdb23b097
Masita770 <ozon777ym12@gmail.com>
2024-05-11 22:33:42 +0900

    fix: ProductController.java 修正

---
+++ src/main/java/com/example/controller/OrdersController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+
+import com.example.domain.Orders;
+import com.example.domain.Products;
+import com.example.domain.Stocks;
+import com.example.service.OrderService;
+import com.example.service.StocksService;
+import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/product")
+public class OrdersController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping("orderManagement")
+    public String newProducts(@RequestBody(required = false) @ModelAttribute Products products) {
+        return "product/orderManagement";
+    }
+    // 3/3 edit遷移がうまく行かない
+    @PostMapping("orderEdit")
+    public String add(@ModelAttribute Orders products, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "product/orderError";
+        }
+        orderService.createOrder(products);
+        return "redirect:orderEdit";
+    }
+}
