2a4dd8e7128c9592c86af21fa8ffbae4037a81c7
Masita770 <ozon777ym12@gmail.com>
2024-05-19 19:50:37 +0900

    fix: date.sql schema.sql 修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,36 @@
-/*INSERT INTO users(
+INSERT INTO products(
     id,
-    username,
-    password
+    p_number,
+    product,
+    category,
+    price,
+    registration_date
     ) VALUES (
     1,
-    'nakazima',
-    3324
-    );*/
+    1,
+    'オフィスデスク',
+    '机',
+    '30,000'
+    );
+
+INSERT INTO stocks(
+    id,
+    product_id,
+    inventory,
+    ) VALUES (
+    1,
+    1,
+    5,
+    );
+
+INSERT INTO orders(
+    id,
+    stocks_id,
+    orders_number,
+    delivery_date
+    ) VALUES (
+    1,
+    1,
+    5,
+    '2024-06-12'
+    );
--- src/main/resources/shcema.sql
+++ src/main/resources/shcema.sql
@@ -1,6 +1,30 @@
-CREATE TABLE IF NOT EXISTS users(
-	id int unsigned NOT NULL AUTO_INCREMENT comment 'USERID',
-	username varchar(100) NOT NULL comment 'ユーザーネーム',
-	password varchar(100) NOT NULL comment 'パスワード',
+CREATE TABLE IF NOT EXISTS product(
+    id int(11) NOT NULL AUTO_INCREMENT,
+	p_number varchar(256) NOT NULL comment '製品番号',
+	category varchar(100) NOT NULL comment '種類',
+	price varchar(256) not null comment '値段',
+	registration_date not null comment '登録日'
+	deleted_date timestamp comment '削除日'
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS stocks(
+    id int(11) NOT NULL AUTO_INCREMENT,
+    product_id int(11) NOT NULL comment '製品番号',
+    inventory char(100) NOT NULL comment '在庫数',
+    update_date timestamp comment '更新日時',
+    PRIMARY KEY(id),
+    FOREIGN KEY product_id
+    REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+    id int(11) NOT NULL AUTO_INCREMENT,
+    stocks_id int(11) NOT NULL comment '在庫管理番号',
+    orders_number char(100) NOT NULL comment '発注数',
+    order_date timestamp comment '発注日',
+    delivery_date date NOT NULL '引き渡し日',
+    PRIMARY KEY(id),
+    FOREIGN KEY stocks_id
+    REFERENCES stocks(id)
 );
